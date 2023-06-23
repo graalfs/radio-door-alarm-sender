be62b9555b1706e4a259573900ea1a847155ce94
Heinz Graalfs <61154537+graalfs@users.noreply.github.com>
2023-06-23 12:38:51 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
 })
 radio.setGroup(19)
+radio.setTransmitPower(7)
 basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) > 500) {
         radio.sendString("C")
     } else {
         radio.sendString("O")
     }
-    basic.pause(2000)
+    basic.pause(5000)
 })
